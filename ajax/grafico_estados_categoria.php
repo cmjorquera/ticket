77faaabada9a220ba4087d7d748aed6830db793f
Cristian Jorquera <cm.jorquerag@gmail.com>
2026-09-09 16:34:19 -0300

    una pausqwea

---
+++ ajax/grafico_estados_categoria.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+require_once __DIR__ . '/_bootstrap.php';
+require_once __DIR__ . '/../clases/DashboardTickets.php';
+
+Sesion::requerir();
+
+try {
+    $db = Conexion::getInstance('sistema_panel_central');
+    $dashboard = new DashboardTickets($db, (int) $_SESSION['id']);
+    $perfil = $dashboard->resolverPerfil((string) ($_GET['perfil'] ?? ($_SESSION['dashboard_perfil'] ?? '')));
+    responder_json(['ok' => true, 'data' => $dashboard->obtenerGraficoCategorias($perfil)]);
+} catch (Throwable $ex) {
+    responder_json(['ok' => false, 'mensaje' => 'No fue posible cargar el gráfico por categoría.'], 500);
+}
